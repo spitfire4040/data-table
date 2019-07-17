24ee8a3ed116417d4c5785ae81887fdef2914230
Jay Thom <jay@Jays-MacBook-Pro.local>
2019-07-16 19:38:16 -0700

    max the wizard

--- src/ReactTableComponent.js
+++ src/ReactTableComponent.js
@@ -7,24 +7,36 @@ const columns = [
   {
     Header: 'UUID',
     accessor: 'uuid',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'URL',
     accessor: 'url',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'SSH_Version',
     accessor: 'ssh_version',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Filename',
     accessor: 'filename',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -32,6 +44,9 @@ const columns = [
   {
     Header: 'Encr_Algo',
     accessor: 'encr_algo',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -39,24 +54,36 @@ const columns = [
   {
     Header: 'Latitude',
     accessor: 'latitude',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Longitude',
     accessor: 'longitude',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Data',
     accessor: 'data',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Timezone',
     accessor: 'timezone',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -64,6 +91,9 @@ const columns = [
   {
     Header: 'Hostname',
     accessor: 'hostname',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -71,24 +101,36 @@ const columns = [
   {
     Header: 'Country_Code3',
     accessor: 'country_code3',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Timestamp',
     accessor: 'timestamp',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Mac_Algo',
     accessor: 'mac_algo',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'ipv4',
     accessor: 'ipv4',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -96,6 +138,9 @@ const columns = [
   {
     Header: 'Continent_Code',
     accessor: 'continent_code',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -103,24 +148,36 @@ const columns = [
   {
     Header: 'Country_Code2',
     accessor: 'country_code2',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'City_Name',
     accessor: 'city_name',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Port',
     accessor: 'port',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Password',
     accessor: 'password',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -128,6 +185,9 @@ const columns = [
   {
     Header: 'sha256',
     accessor: 'sha256',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -135,24 +195,36 @@ const columns = [
   {
     Header: 'Kex_Algo',
     accessor: 'kex_algo',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Region_Code',
     accessor: 'region_code',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Hash',
     accessor: 'hash',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Username',
     accessor: 'username',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -160,6 +232,9 @@ const columns = [
   {
     Header: 'Comp_Algo',
     accessor: 'comp_algo',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -167,24 +242,36 @@ const columns = [
   {
     Header: 'Event_Type',
     accessor: 'event_type',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Protocol',
     accessor: 'protocol',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Country_Name',
     accessor: 'country_name',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
   },
   {
     Header: 'Pub_Key_Algo',
     accessor: 'pub_key_algo',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -192,6 +279,9 @@ const columns = [
   {
     Header: 'Region_Name',
     accessor: 'region_name',
+    Cell: row => (
+      <div title={row.value}> {row.value != null ? 'x': ''} </div>
+    ),
     headerStyle: { whiteSpace: 'unset' },
     style: { whiteSpace: 'unset' },
     maxWidth: 150,
@@ -199,17 +289,44 @@ const columns = [
 ];
 
 export default class ReactTableComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
   render() {
     return (
       <div style={{ padding: '50px' }}>
         <ReactTable
-          manual
-          minRows={0}
-          pageSize={1}
+          defaultPageSize={50}
           data={data}
+          style={{
+            height: (this.state.height*0.9)+"px" // This will force the table body to overflow and scroll, since there is not enough room
+          }}
           columns={columns}
-          pages={0}
           showPagination={true}
+          // SubComponent={row => {
+          //   return (
+          //     <div>
+          //       max
+          //     </div>
+          //   )
+          // }}
         />
       </div>
     );
